e2d3f32a519dd6a2856fc35a173ec688a05cf21c
Michelle <mpuschkarow@web.de>
2025-08-24 00:03:08 +0200

    hero section with split text and scroll trigger animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/navbar.jsx"
+import Hero from './components/hero.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,6 +10,8 @@ const App = () => {
     return (
         <main>
             <Navbar />
+            <Hero />
+            <div className="h-dvh bg-black"></div>
         </main>
     )
 }
