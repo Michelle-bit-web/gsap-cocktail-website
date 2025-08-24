2d39d076610fd57aa001adcacb8dcd32d48dcb6a
Michelle <mpuschkarow@web.de>
2025-08-24 09:31:51 +0200

    cocktail section added

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
                 scrub: true,
             }
         })
-            .to('.right-leaf', { y: 200}, 0)
+            .to('.right-leaf', {y: -100}, 0)
             .to('.left-leaf', {y: -200}, 0)
 
         const startValue = isMobile ? 'top 50%' : 'center 60%'
@@ -93,8 +93,8 @@ const Hero = () => {
                 <video
                 ref={videoRef}
                 src="/videos/output.mp4"
-                muted="true"
-                playsInline="true"
+                muted
+                playsInline
                 preload="auto"
                 />
             </div>
