f70ff9b8c18153981d299501acba5f290e14d8f7
Michelle <mpuschkarow@web.de>
2025-08-26 09:32:42 +0200

    menu cocktails sliding in

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,15 +1,50 @@
 'use client';
 
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import { sliderLists } from "../../constants/index.js";
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
 
 const Menu = () => {
+    const contentRef = useRef();
     const [currentIndex, setCurrentIndex] = useState(0);
     const totalCocktails = sliderLists.length;
     const goToSlide = (index) => {
         const newIndex = (index + totalCocktails) % totalCocktails;
         setCurrentIndex(newIndex);
     }
+
+    const getCocktailAt = (indexOffset) => {
+        return sliderLists[(currentIndex + indexOffset + totalCocktails) % totalCocktails];
+    }
+    const currentCocktail = getCocktailAt(0);
+    const prevCocktail = getCocktailAt(-1);
+    const nextCocktail = getCocktailAt(1);
+
+    useGSAP(() => {
+
+
+        gsap.fromTo('#title', {opacity: 0}, {opacity: 1, duration: 1});
+        gsap.fromTo('.cocktail img', {opacity: 0, xPercent: -100}, {xPercent: 0, opacity: 1, duration: 1, ease: 'power1.inOut'});
+        gsap.fromTo('.details h2', {yPercent: 100, opacity: 0}, {yPercent: 0, opacity: 1, duration: 1, ease: 'power1.inOut'});
+        gsap.fromTo('.details p', {yPercent: 100, opacity: 0}, {yPercent: 0, opacity: 1, duration: 1, ease: 'power1.inOut'});
+    }, [currentIndex]);
+
+    useGSAP(() => {
+        const parallaxTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#menu',
+                start: 'top 30%',
+                end: 'bottom 80%',
+                scrub: true,
+            }
+        })
+
+        parallaxTimeline
+            .from('#m-left-leaf', {x: -100, y: 100})
+            .from('#m-right-leaf', {x: 100, y: 100})
+    })
+
     return (
         <section id="menu" aria-labelledby="menu-heading">
             <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf"/>
@@ -33,13 +68,26 @@ const Menu = () => {
             <div className="content">
                 <div className="arrows">
                     <button className="text-left" onClick={() => goToSlide(currentIndex - 1)}>
-                        <span>previous cockail</span>
+                        <span>{prevCocktail.name}</span>
                         <img src="/images/right-arrow.png" alt="right-arrow" aria-hidden="true"/>
                     </button>
                     <button className="text-left" onClick={() => goToSlide(currentIndex + 1)}>
-                        <span>next cockail</span>
+                        <span>{nextCocktail.name}</span>
                         <img src="/images/left-arrow.png" alt="left-arrow" aria-hidden="true"/>
                     </button>
+                </div>
+                <div className="cocktail">
+                    <img src={currentCocktail.image} className="object-contain" alt={currentCocktail.name}/>
+                </div>
+                <div className="recipe">
+                    <div ref={contentRef} className="info">
+                        <p>Recipe for:</p>
+                        <p id="title">{currentCocktail.name}</p>
+                    </div>
+                    <div className="details">
+                        <h2>{currentCocktail.title}</h2>
+                        <p>{currentCocktail.description}</p>
+                    </div>
                 </div>
             </div>
         </section>
